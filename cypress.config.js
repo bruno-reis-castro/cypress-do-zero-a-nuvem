b594aa3921ae95f853e179d6a49dcec5af176cd8
bruno-reis-castro <bruno.reis.castro1@gmail.com>
2025-05-24 10:05:08 -0300

    Configure cypress cloud

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,11 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+  projectId: "4rj2gk",
   viewportHeight: 880,
   viewportWidth: 1280,
   e2e: {},
   //video: true,
 })
+
+projectId: "4rj2gk"
